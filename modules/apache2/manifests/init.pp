50910650777403347065eedcb066ba451228b5d9
Bohong Li <a60814billy@gmail.com>
2021-09-05 00:02:07 +0000

    add apache2 module

---
+++ modules/apache2/manifests/init.pp
@@ -0,0 +1,11 @@
+# Class: apache 2
+class apache2 {
+  package { 'apache2':
+    ensure => installed
+  }
+  service { 'apache2':
+    ensure  => true,
+    enable  => true,
+    require => Package['apache2']
+  }
+}
